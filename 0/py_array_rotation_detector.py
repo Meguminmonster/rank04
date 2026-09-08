e02c23e9256aa16e107fd2b066f23cb4242292bd
Meguminmonster <114344458+Meguminmonster@users.noreply.github.com>
2026-09-08 13:11:21 +0200

    Delete 0 directory

--- 0/py_array_rotation_detector.py
+++
@@ -1,8 +0,0 @@
-def array_rotation_detector(arr1: list, arr2: list) -> bool:
-
-	return sorted(arr1) == sorted(arr2)
-
-if __name__ == "__main__":
-	print(array_rotation_detector([1, 2, 3, 4, 5, 8], [4, 5, 1, 2, 3]))
-	print(array_rotation_detector([1, 2, 3, 4, 5], [4, 5, 1, 2, 3]))
-	print(array_rotation_detector([], []))
